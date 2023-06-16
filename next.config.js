e345706ccaae482fdc414a9b92b0620b34bc9504
Bond <shrekuu@gmail.com>
2023-06-16 16:59:56 +0800

    switch to yarn; ignore build type checking, eslinting

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     // Required:
     appDir: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // disable temporarily!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
